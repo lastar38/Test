87c437f3455d92e50cddcc1f74d076036b165356
lastar38 <lastarmain@gmail.com>
2021-02-13 14:57:42 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length >= 1 && args[0] == "-tom")
+            {
+                PrintFeetMeterList(1, 10);
+            }
+            else
+            {
+                PrintMeterToFeetList(1, 10);
+            }
+            Console.ReadLine();
+        }
+
+        static void PrintFeetMeterList(int start, int stop)
+        {
+            for(int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetConverter.ToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for(int meter = start; meter <= stop; meter++)
+            {
+                double feet = FeetConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+    }
+}
