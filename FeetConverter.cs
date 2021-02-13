95f6a198250895d104d1244a69f11f781a0d00e2
lastar38 <lastarmain@gmail.com>
2021-02-13 15:02:25 +0900

    first commit

---
+++ FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    public static class FeetConverter
+    {
+        private const double ratio = 0.3048;
+
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+
+        public static double ToMeter(double feet)
+        {
+            return feet * ratio;
+        }
+    }
+}
